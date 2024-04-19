4ada7e00a59e47b6e460e4e90e7d78102dd26b40
Casmir Okorie <casmir456@gmail.com>
2024-04-19 11:49:30 +0100

    updating

--- tailwind.config.js
+++ tailwind.config.js
@@ -60,6 +60,8 @@ module.exports = {
 
         '2xl': '1536px',
         // => @media (min-width: 1536px) { ... }
+        '3xl': '1535px',
+        // => @media (min-width: 1536px) { ... }
 
         
       } 
